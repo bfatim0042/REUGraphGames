f8a00953d51d7603e975228356d9eb22854a07db
jakinng <jakin.ng03@gmail.com>
2024-06-10 11:34:48 -0400

    starting small test embedding triangle into 1x2 lattice

--- CHSH.py
+++ CHSH.py
@@ -1,91 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
-
-
 import numpy as np
-from toqito.states import basis
 
-#The basis: {|0>, |1>}:
-e_0, e_1 = basis(2, 0), basis(2, 1)
-
-
-# In[38]:
-
-
-epr = bell(0)
-
-
-# In[45]:
-
-
-#going through CHSH
+# CHSH as a nonlocal game
 prob_mat = np.array([[1 / 4, 1 / 4], [1 / 4, 1 / 4]])
 
-
-# In[47]:
-
-
-dim_in_alice, dim_out_alice = 2,2
-dim_in_bob, dim_out_bob = 2,2
+dim_in_alice, dim_out_alice = 2, 2
+dim_in_bob, dim_out_bob = 2, 2
 
 
-# In[49]:
-
-
-pred_mat = np.zeros((dim_in_alice, dim_out_alice, dim_in_bob, dim_out_bob))
-
-
-# In[52]:
-
-
-for a_alice in range (dim_out_alice):
-        for b_bob in range (dim_out_bob):
-            for x_alice in range (dim_in_alice):
-                for y_bob in range (dim_in_bob):
-                    if a_alice^b_bob == x_alice * y_bob:
+def chsh_pred_mat():
+    pred_mat = np.zeros((dim_in_alice, dim_out_alice, dim_in_bob, dim_out_bob))
+    for a_alice in range(dim_out_alice):
+        for b_bob in range(dim_out_bob):
+            for x_alice in range(dim_in_alice):
+                for y_bob in range(dim_in_bob):
+                    if a_alice ^ b_bob == x_alice * y_bob:
                         pred_mat[a_alice, b_bob, x_alice, y_bob] = 1
-
-
-# In[53]:
-
-
-
-
-
-# In[55]:
+    return pred_mat
 
 
 from toqito.nonlocal_games.nonlocal_game import NonlocalGame
+
+pred_mat = chsh_pred_mat()
 chsh = NonlocalGame(prob_mat, pred_mat)
+# print(f"{chsh.quantum_value_lower_bound()=}")
+# print(f"{chsh.nonsignaling_value()=}")
+print(f"{chsh.nonsignaling_value()=}")
+print(f"{chsh.quantum_value_lower_bound()=}")
+print(pred_mat)
+print(f"{chsh.classical_value()=}")
+print(pred_mat)
 
+# print(f"{chsh.quantum_value_lower_bound()=}")
+# chsh = NonlocalGame(prob_mat, chsh_pred_mat())
+print(f"{chsh.nonsignaling_value()=}")
+# chsh = NonlocalGame(prob_mat, chsh_pred_mat())
+print(f"{chsh.quantum_value_lower_bound()=}")
 
-# In[58]:
+# ## Use XOR game framework
+# from toqito.nonlocal_games.xor_game import XORGame
 
+# prob_mat = np.array([[1 / 4, 1 / 4], [1 / 4, 1 / 4]])
+# pred_mat = np.array([[0, 0], [0, 1]])
+# chsh = XORGame(prob_mat, pred_mat)
 
-chsh.quantum_value_lower_bound()
-
-
-# In[65]:
-
-
-#easier way for CHSH
-
-from toqito.nonlocal_games.xor_game import XORGame
-prob_mat = np.array([[1/4, 1/4],
-                     [1/4, 1/4]])
-pred_mat = np.array([[0, 0],
-                    [0, 1]])
-chsh = XORGame(prob_mat, pred_mat)
-
-print(chsh.classical_value())
-
-print(chsh.quantum_value())
-
-
-# In[ ]:
-
-
-
-
+# print(f"{chsh.classical_value()=}")
+# print(f"{chsh.quantum_value()=}")
+# print(f"{chsh.nonsignaling_value()=}")
