3ec1e2c1e34f4e0e0c22401ec880297ee6b757b6
jakinng <jakin.ng03@gmail.com>
2024-06-10 13:20:02 -0400

    create question and answer sets

--- triangle-test.py
+++ triangle-test.py
@@ -1,1 +1,22 @@
 import numpy as np
+
+S = [(1, 2), (1, 3), (2, 3)]
+T = S
+
+# Create lattice containing all pairs of coordinates
+k = 1
+l = 2
+
+LX, LY = np.meshgrid(np.arange(k), np.arange(l))
+L = np.vstack((LX.flatten(), LY.flatten())).T
+
+A = []
+for i in range(L.shape[0]):
+    for j in range(L.shape[0]):
+        if i != j:
+            p1 = L[i]
+            p2 = L[j]
+            A.append([p1, p2])
+A = np.array(A)
+B = A
+print(A)
